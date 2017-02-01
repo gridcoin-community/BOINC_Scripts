e6558b44492c7025c16459a440801094ef4cf4b2
Peppernrino <Peppernrino@users.noreply.github.com>
2017-02-01 15:06:19 -0600

    Update HostUserTeamGZ.sh
    added SSL wherever possible.
    removed trailing spaces.
    removed BURP.

--- BOINC stats/Overall/Scripts/HostUserTeamGZ.sh
+++ BOINC stats/Overall/Scripts/HostUserTeamGZ.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "SETI start"
 cd ./SETI
-wget -N --tries=2 http://setiweb.ssl.berkeley.edu/stats/host.gz
-wget -N --tries=2 http://setiweb.ssl.berkeley.edu/stats/team.gz
-wget -N --tries=2 http://setiweb.ssl.berkeley.edu/stats/user.gz
+wget -N --tries=2 https://setiweb.ssl.berkeley.edu/stats/host.gz
+wget -N --tries=2 https://setiweb.ssl.berkeley.edu/stats/team.gz
+wget -N --tries=2 https://setiweb.ssl.berkeley.edu/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -18,9 +18,9 @@ echo "SETI end"
 
 echo "Einstein start"
 cd ../Einstein
-wget -N --tries=2 http://einstein.phys.uwm.edu/stats/host_id.gz
-wget -N --tries=2 http://einstein.phys.uwm.edu/stats/team_id.gz
-wget -N --tries=2 http://einstein.phys.uwm.edu/stats/user_id.gz
+wget -N --tries=2 https://einstein.phys.uwm.edu/stats/host_id.gz
+wget -N --tries=2 https://einstein.phys.uwm.edu/stats/team_id.gz
+wget -N --tries=2 https://einstein.phys.uwm.edu/stats/user_id.gz
 echo "Downloads finished"
 gunzip host_id.gz
 mv host_id host_id.xml
@@ -35,9 +35,9 @@ echo "Einstein end"
 
 echo "Rosetta start"
 cd ../Rosetta
-wget -N --tries=2 http://boinc.bakerlab.org/rosetta/stats/host.gz
-wget -N --tries=2 http://boinc.bakerlab.org/rosetta/stats/team.gz
-wget -N --tries=2 http://boinc.bakerlab.org/rosetta/stats/user.gz
+wget -N --tries=2 https://boinc.bakerlab.org/rosetta/stats/host.gz
+wget -N --tries=2 https://boinc.bakerlab.org/rosetta/stats/team.gz
+wget -N --tries=2 https://boinc.bakerlab.org/rosetta/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -50,28 +50,11 @@ mv team team.xml
 clamscan
 echo "Rosetta end"
 
-echo "BURP start"
-cd ../BURP
-wget -N --tries=2 http://burp.boinc.dk/stats/host_id.gz
-wget -N --tries=2 http://burp.boinc.dk/stats/team_id.gz
-wget -N --tries=2 http://burp.boinc.dk/stats/user_id.gz
-echo "Downloads finished"
-gunzip host_id.gz
-mv host_id host_id.xml
-clamscan
-gunzip user_id.gz
-mv user_id user_id.xml
-clamscan
-gunzip team_id.gz
-mv team_id team_id.xml
-clamscan
-echo "BURP end"
-
 echo "PrimeGrid start"
 cd ../PrimeGrid
-wget -N --tries=2 http://www.primegrid.com/stats/host.gz
-wget -N --tries=2 http://www.primegrid.com/stats/team.gz
-wget -N --tries=2 http://www.primegrid.com/stats/user.gz
+wget -N --tries=2 https://www.primegrid.com/stats/host.gz
+wget -N --tries=2 https://www.primegrid.com/stats/team.gz
+wget -N --tries=2 https://www.primegrid.com/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -86,8 +69,8 @@ echo "PrimeGrid end"
 
 echo "SZTAKI start"
 cd ../SZTAKI
-wget -N --tries=2 http://szdg.lpds.sztaki.hu/szdg/stats/host.gz 
-wget -N --tries=2 http://szdg.lpds.sztaki.hu/szdg/stats/team.gz 
+wget -N --tries=2 http://szdg.lpds.sztaki.hu/szdg/stats/host.gz
+wget -N --tries=2 http://szdg.lpds.sztaki.hu/szdg/stats/team.gz
 wget -N --tries=2 http://szdg.lpds.sztaki.hu/szdg/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
@@ -134,9 +117,9 @@ echo "Leiden end"
 
 echo "MooWrap start"
 cd ../MooWrap
-wget -N --tries=2 http://moowrap.net/stats/host.gz
-wget -N --tries=2 http://moowrap.net/stats/team.gz
-wget -N --tries=2 http://moowrap.net/stats/user.gz
+wget -N --tries=2 https://moowrap.net/stats/host.gz
+wget -N --tries=2 https://moowrap.net/stats/team.gz
+wget -N --tries=2 https://moowrap.net/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -168,9 +151,9 @@ echo "GPUGRID end"
 
 echo "Asteroids start"
 cd ../Asteroids
-wget -N --tries=2 http://asteroidsathome.net/boinc/stats/host.gz
-wget -N --tries=2 http://asteroidsathome.net/boinc/stats/team.gz
-wget -N --tries=2 http://asteroidsathome.net/boinc/stats/user.gz
+wget -N --tries=2 https://asteroidsathome.net/boinc/stats/host.gz
+wget -N --tries=2 https://asteroidsathome.net/boinc/stats/team.gz
+wget -N --tries=2 https://asteroidsathome.net/boinc/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -185,9 +168,9 @@ echo "Asteroids end"
 
 echo "Milkyway start"
 cd ../Milkyway
-wget -N --tries=2 http://milkyway.cs.rpi.edu/milkyway/stats/host.gz
-wget -N --tries=2 http://milkyway.cs.rpi.edu/milkyway/stats/team.gz
-wget -N --tries=2 http://milkyway.cs.rpi.edu/milkyway/stats/user.gz
+wget -N --tries=2 https://milkyway.cs.rpi.edu/milkyway/stats/host.gz
+wget -N --tries=2 https://milkyway.cs.rpi.edu/milkyway/stats/team.gz
+wget -N --tries=2 https://milkyway.cs.rpi.edu/milkyway/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -202,9 +185,9 @@ echo "Milkyway end"
 
 echo "Collatz start"
 cd ../Collatz
-wget -N --tries=2 http://boinc.thesonntags.com/collatz/stats/host.gz
-wget -N --tries=2 http://boinc.thesonntags.com/collatz/stats/team.gz
-wget -N --tries=2 http://boinc.thesonntags.com/collatz/stats/user.gz
+wget -N --tries=2 https://boinc.thesonntags.com/collatz/stats/host.gz
+wget -N --tries=2 https://boinc.thesonntags.com/collatz/stats/team.gz
+wget -N --tries=2 https://boinc.thesonntags.com/collatz/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -219,9 +202,9 @@ echo "Collatz end"
 
 echo "YAFU start"
 cd ../YAFU
-wget -N --tries=2 http://yafu.myfirewall.org/yafu/stats/host.gz
-wget -N --tries=2 http://yafu.myfirewall.org/yafu/stats/team.gz
-wget -N --tries=2 http://yafu.myfirewall.org/yafu/stats/user.gz
+wget -N --tries=2 https://yafu.myfirewall.org/yafu/stats/host.gz
+wget -N --tries=2 https://yafu.myfirewall.org/yafu/stats/team.gz
+wget -N --tries=2 https://yafu.myfirewall.org/yafu/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -236,9 +219,9 @@ echo "YAFU end"
 
 echo "Cosmology start"
 cd ../Cosmology
-wget -N --tries=2 http://www.cosmologyathome.org/stats/host.gz
-wget -N --tries=2 http://www.cosmologyathome.org/stats/team.gz
-wget -N --tries=2 http://www.cosmologyathome.org/stats/user.gz
+wget -N --tries=2 https://www.cosmologyathome.org/stats/host.gz
+wget -N --tries=2 https://www.cosmologyathome.org/stats/team.gz
+wget -N --tries=2 https://www.cosmologyathome.org/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -295,9 +278,9 @@ echo "NFS end"
 
 echo "NumbersFields start"
 cd ../NumbersFields
-wget -N --tries=2 http://numberfields.asu.edu/NumberFields/stats/host.gz
-wget -N --tries=2 http://numberfields.asu.edu/NumberFields/stats/team.gz
-wget -N --tries=2 http://numberfields.asu.edu/NumberFields/stats/user.gz
+wget -N --tries=2 https://numberfields.asu.edu/NumberFields/stats/host.gz
+wget -N --tries=2 https://numberfields.asu.edu/NumberFields/stats/team.gz
+wget -N --tries=2 https://numberfields.asu.edu/NumberFields/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
@@ -329,9 +312,9 @@ echo "Skynet end"
 
 echo "YOYO start"
 cd ../YOYO
-wget -N --tries=2 http://www.rechenkraft.net/yoyo/stats/host.gz
-wget -N --tries=2 http://www.rechenkraft.net/yoyo/stats/team.gz
-wget -N --tries=2 http://www.rechenkraft.net/yoyo/stats/user.gz
+wget -N --tries=2 https://www.rechenkraft.net/yoyo/stats/host.gz
+wget -N --tries=2 https://www.rechenkraft.net/yoyo/stats/team.gz
+wget -N --tries=2 https://www.rechenkraft.net/yoyo/stats/user.gz
 echo "Downloads finished"
 gunzip host.gz
 mv host host.xml
